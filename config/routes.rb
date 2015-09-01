be3bd2304f2c09225eb31f9d7b80e9c4de9d0d49
Maria <unamaria@gmail.com>
2015-09-01 14:38:33 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'  
+  get 'signup'  => 'users#new'
+  resources :users  
 end
