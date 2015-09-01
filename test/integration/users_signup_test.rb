dcc3ed774c7115679c46bbc0b351dfadf565e413
Maria <unamaria@gmail.com>
2015-09-01 15:24:07 -0500

    Improve tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger'
   end
 
   test "valid signup information" do
@@ -22,6 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
   
 end
